5dd4fd3cf3715026a688b27236fa4880ef29a4a6
Qiuchi-Hu <joyce.hu2019z@gmail.com>
2020-11-26 15:40:57 -0800

    first update on actions and reducers

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,9 +1,11 @@
 export default (state =[], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
-            return posts;
+            return action.payload;
+        case 'CREATE':
+            return [...posts, action.payload];
+
         default:
             return posts;
-
     }
 }
