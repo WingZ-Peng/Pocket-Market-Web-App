24b02de9ed9d5b957774e5d2611eea3055b21633
hoyincheung <hoyincheung44@gmail.com>
2020-11-27 02:14:12 -0800

    front-end update

--- client/actions/posts.js
+++ client/actions/posts.js
@@ -1,4 +1,6 @@
-import * as api from '../api';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
+import * as api from '../api/index.js';
 
 //action creators 
 export const getPosts =() => async(dispatch)=>{
@@ -8,7 +10,7 @@ export const getPosts =() => async(dispatch)=>{
     }catch(error){
         console.log(error.message);
     }
-}
+};
 
 export const createPost = (post) => async(dispatch)=>{
     try {
@@ -16,8 +18,36 @@ export const createPost = (post) => async(dispatch)=>{
 
         dispatch({type: 'CREATE',payload: data});
     }catch(error){
-        console.log(error);
+        console.log(error.message);
     }
+};
 
-    
-}
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+      const { data } = await api.updatePost(id, post);
+  
+      dispatch({ type: UPDATE, payload: data });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+  export const likePost = (id) => async (dispatch) => {
+    try {
+      const { data } = await api.likePost(id);
+  
+      dispatch({ type: LIKE, payload: data });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+  export const deletePost = (id) => async (dispatch) => {
+    try {
+      await await api.deletePost(id);
+  
+      dispatch({ type: DELETE, payload: id });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
--- client/index.js
+++ client/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, componse} from 'redux';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
 
+import reducers from './reducers';
 import App from './App';
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+import './index.css';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store = {store}>
         <App/>
     </Provider>,
- document.getElementById('root'));
+ document.getElementById('root'),
+ );
